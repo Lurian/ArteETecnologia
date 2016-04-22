a4607712124f463eccf0ab3b298e83b938931793
lurian <luclurian@gmail.com>
2016-04-21 23:12:17 -0300

    Super Refact Bye insta Hi QRC

--- app/app.js
+++ app/app.js
@@ -6,7 +6,9 @@ angular.module('myApp', [
   'myApp.view1',
   'myApp.view2',
   'myApp.version',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ja.qr',
+  'ngAnimate'
 ]).
 config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 }]);
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -14,77 +14,25 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', function($scope, $window, $location) {
-  $scope.myInterval = 5000;
-  $scope.noWrapSlides = false;
-  $scope.active = 0;
-  $scope.access_token;
 
-  var slides = $scope.slides = [];
-  var currIndex = 0;
+  $scope.showImage = false;
+  $scope.showQR = false;
 
-  $scope.addSlide = function() {
-    var newWidth = 600 + slides.length + 1;
-    slides.push({
-      image: 'http://lorempixel.com/' + newWidth + '/300',
-      text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
-      id: currIndex++
-    });
-  };
+  $scope.stringqr = '';
 
-  $scope.redirectInstagram = function(){
-    $window.location.href = 'https://api.instagram.com/oauth/authorize/?client_id=4ffeb7f0f089438887d9413010d67708' +
-    '&redirect_uri=http://localhost:8000/app/?this=&response_type=token';
-  }
-
-  $scope.showUrl = function(){
-    console.log($location.absUrl());
-  }
-
-  $scope.getAccessToken = function(){
-    var urlSplit = $location.absUrl().split("access_token=");
-    var token = urlSplit[urlSplit.length -1];
-     $scope.access_token = token;
-     console.log(token);
-  }
-
-  $scope.randomize = function() {
-    var indexes = generateIndexesArray();
-    assignNewIndexesToSlides(indexes);
-  };
-
-  for (var i = 0; i < 4; i++) {
-    $scope.addSlide();
-  }
-
-  // Randomize logic below
-
-  function assignNewIndexesToSlides(indexes) {
-    for (var i = 0, l = slides.length; i < l; i++) {
-      slides[i].id = indexes.pop();
-    }
-  }
-
-  function generateIndexesArray() {
-    var indexes = [];
-    for (var i = 0; i < currIndex; ++i) {
-      indexes[i] = i;
-    }
-    return shuffle(indexes);
-  }
-
-  // http://stackoverflow.com/questions/962802#962890
-  function shuffle(array) {
-    var tmp, current, top = array.length;
-
-    if (top) {
-      while (--top) {
-        current = Math.floor(Math.random() * (top + 1));
-        tmp = array[current];
-        array[current] = array[top];
-        array[top] = tmp;
-      }
-    }
-
-    return array;
-  }
+  $scope.isImage = function() {
+    var image = new Image();
+    console.log("show imagem: " +  $scope.showImage)
+    image.onerror = function() {
+         $scope.showImage = false;
+           $scope.showQR = false;
+         $scope.$apply();
+    };
+    image.onload = function() {
+         $scope.showImage = true;
+         $scope.showQR = true;
+         $scope.$apply();
+    };
+    image.src = $scope.stringqr;
+}
 });
